06fe5d202c941fc3cd74981d31231ac14039dc2c
Juliodvp29 <juliodevep29@gmail.com>
2026-04-06 16:03:25 -0500

    feat(layout): create base layout and header

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/layout/header/header";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, CardModule],
+  imports: [RouterOutlet, ButtonModule, CardModule, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
