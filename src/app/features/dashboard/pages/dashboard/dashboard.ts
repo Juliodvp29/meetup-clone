3f3f0a87c975516e81a412b64b922bf1b8285604
Juliodvp29 <juliodevep29@gmail.com>
2026-04-06 22:20:36 -0500

    feat(dashboard): add stats card component

--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DashboardProfileComponent } from '../../components/dashboard-profile/dashboard-profile';
+import { DashboardStatsComponent } from '../../components/dashboard-stats/dashboard-stats';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [DashboardProfileComponent],
+  imports: [DashboardProfileComponent, DashboardStatsComponent],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
